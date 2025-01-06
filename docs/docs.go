76417b7c95e88d2d98ace71634b20f2dd723d49d
Otabek Davronbekov <davronbekov.otabek@gmail.com>
2025-01-06 23:53:51 +0500

    swagg upd

--- docs/docs.go
+++ docs/docs.go
@@ -259,7 +259,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0.0",
-	Host:             "127.0.0.1:8000",
+	Host:             "api.qahvazor.uz",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Qahvazor | Api",
