9c758a5eeaa4f78c6dd42a8dad40a6fc6a3fb11d
Otabek Davronbekov <davronbekov.otabek@gmail.com>
2025-01-07 15:49:37 +0500

    swag update

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 // @title Qahvazor | Api
 // @version 1.0.0
 // @contact.email davronbekov.o@itv.uz
-// @host 127.0.0.1:8000
+// @host api.qahvazor.uz
 // @BasePath /api/v1
 func main() {
 	app := fx.New(
