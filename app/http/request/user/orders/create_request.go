918600d6b7db5aba3db35de144d4b33af264d6b0
Otabek Davronbekov <davronbekov.otabek@gmail.com>
2025-06-26 12:55:27 +0500

    Update create_request.go

--- app/http/request/user/orders/create_request.go
+++ app/http/request/user/orders/create_request.go
@@ -1,6 +1,6 @@
 package user_orders_request
 
 type CreateRequest struct {
-	ShopID  uint `json:"shop_id"`
-	DrinkID uint `json:"drink_id"`
+	ShopID  uint `json:"shopId"`
+	DrinkID uint `json:"drinkId"`
 }
